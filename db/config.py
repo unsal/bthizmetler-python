1421a10b3079fc29251d2006b558796f564fbd80
unsal <unsal@unsal.org>
2018-05-04 15:12:06 +0300

    commit

--- db/config.py
+++ db/config.py
@@ -1,7 +1,7 @@
 # Veritabanı bağlantısı için
 class Config:
-        # HOST = "bt.ozyegin.edu.tr:5432"
-        HOST = "localhost:5432"
+        HOST = "bt.ozyegin.edu.tr:5432"
+        # HOST = "localhost:5432"
         DATABASE = "bthizmetler"
         USER = "postgres"
         PASSWORD = "Qaz1wsx2!"
